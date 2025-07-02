098a73ed09d4725301b59a551093f70294a3e8ec
yukito <yukito@inoueshoutonoMacBook-Air.local>
2025-07-02 21:07:48 +0900

    css completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='formContainer'>
+      <form>
+        <h1>ログインフォーム</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>ユーザー名</label>
+            <input type="text" placeholder='ユーザー名' name='username' />
+          </div>
+          <div className="formField">
+            <label>メールアドレス</label>
+            <input type="text" placeholder='メールアドレス' name='mailAddress' />
+          </div>
+          <div className="formField">
+            <label>パスワード</label>
+            <input type="text" placeholder='パスワード' name='password' />
+          </div>
+          <button className='submitButton'>ログイン</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
